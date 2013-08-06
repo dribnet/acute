bb651f11ae2f8a4beb7b8622b8c98194fe227d87
Tom White <tom@sixdozen.com>
2013-08-05 22:24:34 -0700

    acute upgrades: angular, clojure, mrhyde, cljsbuild

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject acute "0.0.1-SNAPSHOT"
   :description "acute: experimental angular library"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.drib/strokes "0.4.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [net.drib/mrhyde "0.5.0"]]
   :min-lein-version "2.0.0"
   :source-paths ["src/clj" "src/cljs"]
 
-  :plugins [[lein-cljsbuild "0.2.10"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
 
   :cljsbuild {
     :builds [{
-      :source-path "src/cljs"
+      :source-paths ["src/cljs"]
       :compiler {
         :output-to "public/out/acute.js"
         :optimizations :whitespace
